06b2b6b360e01f5d544efa5e56c623b646f0bcc5
moises <elnaufrago2009@gmail.com>
2019-12-06 10:44:44 -0500

    add ppvar login and regiter

--- src/views/auth/Registro.vue
+++ src/views/auth/Registro.vue
@@ -1,5 +1,29 @@
 <template>
   <div>
-    Registro
+
+    <!-- Appbar -->
+    <appbar></appbar>
+
+    <div class="container pt-5 ">
+      <div class="row">
+        <div class="col-9 mx-auto">
+          <b-card header="Registro de Empresa">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+        </div>
+      </div>
+    </div>
+
+
   </div>
 </template>
+
+<script>
+  import Appbar from './_navbar'
+
+  export default {
+    components: {
+      'appbar': Appbar
+    }
+  }
+</script>
---
+++ src/views/auth/_navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- AppBar-->
+  <div>
+    <b-navbar toggleable="lg" type="light" variant="light">
+
+      <b-navbar-brand href="#">
+        <img src="https://placekitten.com/g/30/30" class="d-inline-block align-top" alt="Kitten">
+        Facturacion Electronica
+      </b-navbar-brand>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="login">Login</b-nav-item>
+          <b-nav-item to="registro">Registro</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div> <!-- End AppBar-->
+</template>
