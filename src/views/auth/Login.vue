a535f5241d5163afc29897e5356e18bf8b2b0578
Moises <elnaufrago2009@gmail.com>
2019-12-08 03:59:17 -0500

    login form add

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -2,21 +2,39 @@
   <div>
 
     <div class="container pt-5 ">
-      <div class="row">
-        <div class="col-9 mx-auto">
+      <div class="row ">
+        <div class="col-6 mx-auto">
 
           <!-- Card -->
           <b-card header="Entrar al Sistema">
             <b-card-text>
 
-              <b-row class="mx-auto">
-                <b-col class="col-2">
-                  <label>Correo:</label>
+              <!-- Email -->
+              <b-row class="my-3">
+                <b-col sm="3">
+                  <label for="input-small" class="col-form-label">Email</label>
                 </b-col>
-                <b-col class="col-5">
-                  <b-form-input id="input-small" placeholder="elnaufrago@gmail.com"></b-form-input>
+                <b-col sm="9">
+                  <b-form-input :type="text" placeholder="elnaufrago@gmail.com"></b-form-input>
                 </b-col>
               </b-row>
+
+              <!-- Contrasenia -->
+              <b-row class="my-3">
+                <b-col sm="3">
+                  <label for="input-small" class="col-form-label">Contrasenia</label>
+                </b-col>
+                <b-col sm="9">
+                  <b-form-input :type="password" placeholder="*************"></b-form-input>
+                </b-col>
+              </b-row>
+
+              <b-col>
+                <b-button type="submit" variant="primary" class="offset-3 mr-2"><font-awesome-icon icon="user" /> Entrar</b-button>
+              </b-col>
+
+
+
 
             </b-card-text>
           </b-card>
